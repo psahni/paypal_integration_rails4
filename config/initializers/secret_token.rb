29091b1794759eefa52ede97b1489588e915073b
psahni <prashant.sahni5@gmail.com>
2013-07-15 11:32:52 +0530

    first commit - setup app'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaypalRails4::Application.config.secret_token = 'ecfa157ad7ab426b851f14af6addadee38e1618f965ec6315eeb0def4d7d840b3756c50bfb80b30b03cb02180833d73ba8a8cd4cdfbe4a490309f3c1ec961ad3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PaypalRails4::Application.config.session_store :cookie_store, key: '_paypal_rails4_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PaypalRails4::Application.config.session_store :active_record_store
